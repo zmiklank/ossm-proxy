openshift-service-mesh/proxy

Initial commit for 3.2

ossm/ci/common.sh
@@ -1,0 +1,59 @@
+#!/bin/bash
+
+export CC=clang CXX=clang++ ENVOY_OPENSSL=1
+
+ARCH=$(uname -m)
+if [ "${ARCH}" = "ppc64le" ]; then
+  ARCH="ppc"
+fi
+export ARCH
+
+OUTPUT_TO_IGNORE="\
+INFO: From|\
+Fixing bazel-out|\
+cache:INFO:  - ok|\
+processwrapper-sandbox.*execroot.*io_istio_proxy|\
+proto is unused\
+"
+
+COMMON_FLAGS="\
+    --config=release \
+    --config=${ARCH} \
+"
+
+if [ -n "${BAZEL_REMOTE_CACHE}" ]; then
+  COMMON_FLAGS+=" --remote_cache=${BAZEL_REMOTE_CACHE} "
+elif [ -n "${BAZEL_DISK_CACHE}" ]; then
+  COMMON_FLAGS+=" --disk_cache=${BAZEL_DISK_CACHE} "
+fi
+
+if [ -n "${CI}" ]; then
+  COMMON_FLAGS+=" --config=ci-config "
+
+  # Throttle resources to work for our CI environemt
+  LOCAL_CPU_RESOURCES="${LOCAL_CPU_RESOURCES:-6}"
+  LOCAL_RAM_RESOURCES="${LOCAL_RAM_RESOURCES:-12288}"
+  LOCAL_JOBS="${LOCAL_JOBS:-3}"
+
+  COMMON_FLAGS+=" --local_cpu_resources=${LOCAL_CPU_RESOURCES} "
+  COMMON_FLAGS+=" --local_ram_resources=${LOCAL_RAM_RESOURCES} "
+  COMMON_FLAGS+=" --jobs=${LOCAL_JOBS} "
+fi
+
+function bazel_build() {
+  bazel build \
+    ${COMMON_FLAGS} \
+    "${@}" \
+  2>&1 | grep --line-buffered -v -E "${OUTPUT_TO_IGNORE}"
+}
+
+function bazel_test() {
+  bazel test \
+    ${COMMON_FLAGS} \
+    --build_tests_only \
+    "${@}" \
+  2>&1 | grep --line-buffered -v -E "${OUTPUT_TO_IGNORE}"
+}
+
+# Fix path to the vendor deps
+sed -i "s|=/work/|=$(pwd)/|" ossm/bazelrc-vendor

ossm/ci/pre-submit-envoy.sh
@@ -1,0 +1,62 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+export CC=clang CXX=clang++
+
+# Download the actual WORKSPACE file for Envoy
+SHA=$(sed -n -e 's/OPENSSL_ENVOY_SHA *= *"\(.*\)"/\1/p' WORKSPACE)
+cd ossm/vendor/envoy
+curl -sfLO "https://raw.githubusercontent.com/envoyproxy/envoy-openssl/${SHA}/WORKSPACE"
+
+# Set the bazel flags
+BAZEL_STARTUP_ARGS=${BAZEL_STARTUP_ARGS:-}
+BAZEL_BUILD_ARGS=${BAZEL_BUILD_ARGS:-}
+
+BAZEL_BUILD_ARGS+=" \
+--host_force_python=PY3 \
+--extra_toolchains=@local_jdk//:all \
+--tool_java_runtime_version=local_jdk \
+--config=clang \
+--verbose_failures \
+--test_output=errors \
+--color=no \
+--//bazel:http3=false \
+--define=boringssl=fips \
+--build_tag_filters=-nofips \
+--test_tag_filters=-nofips \
+--action_env=OPENSSL_ROOT_DIR \
+--test_env=ENVOY_IP_TEST_VERSIONS=v4only \
+"
+
+if [ -n "${BAZEL_REMOTE_CACHE:-}" ]; then
+  BAZEL_BUILD_ARGS+=" --remote_cache=${BAZEL_REMOTE_CACHE} "
+elif [ -n "${BAZEL_DISK_CACHE:-}" ]; then
+  BAZEL_BUILD_ARGS+=" --disk_cache=${BAZEL_DISK_CACHE} "
+fi
+
+if [ -n "${CI:-}" ]; then
+  BAZEL_BUILD_ARGS+=" --local_cpu_resources=${LOCAL_CPU_RESOURCES:-6} "
+  BAZEL_BUILD_ARGS+=" --local_ram_resources=${LOCAL_RAM_RESOURCES:-12288} "
+  BAZEL_BUILD_ARGS+=" --jobs=${LOCAL_JOBS:-3} "
+fi
+
+# Make sure the bazel command runs as non-root, otherwise we will get the error
+# "The current user is root, please run as non-root when using the hermetic Python interpreter. See https://github.com/bazelbuild/rules_python/pull/713."
+HELPER=""
+if [ "${EUID}" == "0" ]; then
+  HELPER="runuser -u user -- "
+  chown -R user /work
+fi
+
+# Skip failing tests
+SKIP=" \
+-//test/common/signal:signals_test \
+-//test/extensions/filters/listener/original_dst:original_dst_integration_test \
+"
+
+# Compile the tests first
+${HELPER} bazel ${BAZEL_STARTUP_ARGS} build ${BAZEL_BUILD_ARGS} --build_tests_only //test/... -- ${SKIP}
+
+# Run the tests
+${HELPER} bazel ${BAZEL_STARTUP_ARGS} test ${BAZEL_BUILD_ARGS} --build_tests_only --flaky_test_attempts=3 //test/... -- ${SKIP}

ossm/scripts/update-deps.sh
@@ -1,0 +1,145 @@
+#!/bin/bash
+# Copyright Red Hat, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eo pipefail
+
+export CC=clang CXX=clang++ ENVOY_OPENSSL=1
+
+function init(){
+  ROOT_DIR="$(pwd)"
+
+  OUTPUT_BASE="$(mktemp -d)"
+  VENDOR_DIR="${ROOT_DIR}/ossm/vendor"
+  BAZELRC="${ROOT_DIR}/ossm/bazelrc-vendor"
+
+  rm -rf "${OUTPUT_BASE}" &&  mkdir -p "${OUTPUT_BASE}"
+  rm -rf "${VENDOR_DIR}" &&  mkdir -p "${VENDOR_DIR}"
+  : > "${BAZELRC}"
+
+  # Remove symlinks to previous builds to avoid issues
+  rm -f bazel-*
+
+
+  IGNORE_LIST=(
+        "bazel_tools"
+        "envoy_api"
+        "envoy_build_config"
+        "local_config"
+        "local_jdk"
+        "bazel_gazelle_go"
+        "openssl"
+        "go_sdk"
+        "remotejdk"
+        "rust"
+        "nodejs"
+        "rules_foreign_cc_framework_toolchain_freebsd_commands"
+        "rules_foreign_cc_framework_toolchain_macos_commands"
+        "rules_foreign_cc_framework_toolchain_windows_commands"
+        "emscripten"
+        "python3_12_host"
+        "python3_12_x86_64"
+        "python3_12_ppc"
+        "python3_12_s390x"
+        "python3_12_aarch64"
+  )
+}
+
+function error() {
+  echo "$@"
+  exit 1
+}
+
+function validate() {
+  if [ ! -f "WORKSPACE" ]; then
+    error "Must run in the envoy/proxy dir"
+  fi
+}
+
+function contains () {
+  local e match="$1"
+  shift
+  for e; do [[ "$match" == "$e"* ]] && return 0; done
+  return 1
+}
+
+function copy_files() {
+  local cp_flags
+  for f in "${OUTPUT_BASE}"/external/*; do
+    if [ -d "${f}" ]; then
+      repo_name=$(basename "${f}")
+      if contains "${repo_name}" "${IGNORE_LIST[@]}" ; then
+        continue
+      fi
+
+      cp_flags="-rL"
+      if [ "${repo_name}" == "emscripten_toolchain" ]; then
+        cp_flags="-r"
+      fi
+      cp "${cp_flags}" "${f}" "${VENDOR_DIR}" || echo "Copy of ${f} failed. Ignoring..."
+      echo "build --override_repository=${repo_name}=/work/ossm/vendor/${repo_name}" >> "${BAZELRC}"
+    fi
+  done
+
+
+  chmod -R +w "${VENDOR_DIR}"
+  find "${VENDOR_DIR}" -name .git -type d -print0 | xargs -0 -r rm -rf
+  find "${VENDOR_DIR}" -name .gitignore -type f -delete
+  find "${VENDOR_DIR}" -name __pycache__ -type d -print0 | xargs -0 -r rm -rf
+  find "${VENDOR_DIR}" -name '*.pyc' -delete
+}
+
+function run_bazel() {
+  # Workaround to force fetch of rules_license
+  bazel --output_base="${OUTPUT_BASE}" fetch @remote_java_tools//java_tools/zlib:zlib || true
+
+  # Workaround to force fetch of protoc for arm
+  bazel --output_base="${OUTPUT_BASE}" fetch @com_google_protobuf_protoc_linux_aarch_64//:protoc
+
+  bazel --output_base="${OUTPUT_BASE}" fetch @com_github_gperftools_gperftools//:all
+
+  # Fetch all the rest and check everything using "build --nobuild "option
+  for config in x86_64 aarch64 s390x ppc; do
+    bazel --output_base="${OUTPUT_BASE}" build --nobuild --config="${config}" //...
+  done
+}
+
+function patch_python() {
+  local dir repo_name
+
+  for arch in x86_64 s390x ppc64le aarch64; do
+    repo_name="python3_12_${arch}-unknown-linux-gnu"
+    dir="${VENDOR_DIR}/${repo_name}"
+    /bin/rm -rf "${dir}"
+    mkdir -p "${dir}"
+    cp "${ROOT_DIR}/ossm/scripts/BUILD.bazel.python" "${dir}/BUILD.bazel"
+
+    echo "build --override_repository=${repo_name}=/work/ossm/vendor/${repo_name}" >> "${BAZELRC}"
+    echo "workspace(name = \"${repo_name}\")" > "${dir}/WORKSPACE"
+  done
+}
+
+function main() {
+  validate
+  init
+  run_bazel
+  copy_files
+  patch_python
+
+  echo
+  echo "Done. Inspect the result with git status"
+  echo
+}
+
+main

zmiklank/ossm-proxy

ossm/ci/common.sh
@@ -1,0 +1,59 @@
+#!/bin/bash
+
+export CC=gcc CXX=g++ ENVOY_OPENSSL=1
+
+ARCH=$(uname -m)
+if [ "${ARCH}" = "ppc64le" ]; then
+  ARCH="ppc"
+fi
+export ARCH
+
+OUTPUT_TO_IGNORE="\
+INFO: From|\
+Fixing bazel-out|\
+cache:INFO:  - ok|\
+processwrapper-sandbox.*execroot.*io_istio_proxy|\
+proto is unused\
+"
+
+COMMON_FLAGS="\
+    --config=release \
+    --config=${ARCH} \
+"
+
+if [ -n "${BAZEL_REMOTE_CACHE}" ]; then
+  COMMON_FLAGS+=" --remote_cache=${BAZEL_REMOTE_CACHE} "
+elif [ -n "${BAZEL_DISK_CACHE}" ]; then
+  COMMON_FLAGS+=" --disk_cache=${BAZEL_DISK_CACHE} "
+fi
+
+if [ -n "${CI}" ]; then
+  COMMON_FLAGS+=" --config=ci-config "
+
+  # Throttle resources to work for our CI environemt
+  LOCAL_CPU_RESOURCES="${LOCAL_CPU_RESOURCES:-6}"
+  LOCAL_RAM_RESOURCES="${LOCAL_RAM_RESOURCES:-12288}"
+  LOCAL_JOBS="${LOCAL_JOBS:-3}"
+
+  COMMON_FLAGS+=" --local_cpu_resources=${LOCAL_CPU_RESOURCES} "
+  COMMON_FLAGS+=" --local_ram_resources=${LOCAL_RAM_RESOURCES} "
+  COMMON_FLAGS+=" --jobs=${LOCAL_JOBS} "
+fi
+
+function bazel_build() {
+  bazel build \
+    ${COMMON_FLAGS} \
+    "${@}" \
+  2>&1 | grep --line-buffered -v -E "${OUTPUT_TO_IGNORE}"
+}
+
+function bazel_test() {
+  bazel test \
+    ${COMMON_FLAGS} \
+    --build_tests_only \
+    "${@}" \
+  2>&1 | grep --line-buffered -v -E "${OUTPUT_TO_IGNORE}"
+}
+
+# Fix path to the vendor deps
+sed -i "s|=/work/|=$(pwd)/|" ossm/bazelrc-vendor

ossm/ci/pre-submit-envoy.sh
@@ -1,0 +1,62 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+export CC=gcc CXX=g++
+
+# Download the actual WORKSPACE file for Envoy
+SHA=$(sed -n -e 's/OPENSSL_ENVOY_SHA *= *"\(.*\)"/\1/p' WORKSPACE)
+cd ossm/vendor/envoy
+curl -sfLO "https://raw.githubusercontent.com/envoyproxy/envoy-openssl/${SHA}/WORKSPACE"
+
+# Set the bazel flags
+BAZEL_STARTUP_ARGS=${BAZEL_STARTUP_ARGS:-}
+BAZEL_BUILD_ARGS=${BAZEL_BUILD_ARGS:-}
+
+BAZEL_BUILD_ARGS+=" \
+--host_force_python=PY3 \
+--extra_toolchains=@local_jdk//:all \
+--tool_java_runtime_version=local_jdk \
+--config=gcc \
+--verbose_failures \
+--test_output=errors \
+--color=no \
+--//bazel:http3=false \
+--define=boringssl=fips \
+--build_tag_filters=-nofips \
+--test_tag_filters=-nofips \
+--action_env=OPENSSL_ROOT_DIR \
+--test_env=ENVOY_IP_TEST_VERSIONS=v4only \
+"
+
+if [ -n "${BAZEL_REMOTE_CACHE:-}" ]; then
+  BAZEL_BUILD_ARGS+=" --remote_cache=${BAZEL_REMOTE_CACHE} "
+elif [ -n "${BAZEL_DISK_CACHE:-}" ]; then
+  BAZEL_BUILD_ARGS+=" --disk_cache=${BAZEL_DISK_CACHE} "
+fi
+
+if [ -n "${CI:-}" ]; then
+  BAZEL_BUILD_ARGS+=" --local_cpu_resources=${LOCAL_CPU_RESOURCES:-6} "
+  BAZEL_BUILD_ARGS+=" --local_ram_resources=${LOCAL_RAM_RESOURCES:-12288} "
+  BAZEL_BUILD_ARGS+=" --jobs=${LOCAL_JOBS:-3} "
+fi
+
+# Make sure the bazel command runs as non-root, otherwise we will get the error
+# "The current user is root, please run as non-root when using the hermetic Python interpreter. See https://github.com/bazelbuild/rules_python/pull/713."
+HELPER=""
+if [ "${EUID}" == "0" ]; then
+  HELPER="runuser -u user -- "
+  chown -R user /work
+fi
+
+# Skip failing tests
+SKIP=" \
+-//test/common/signal:signals_test \
+-//test/extensions/filters/listener/original_dst:original_dst_integration_test \
+"
+
+# Compile the tests first
+${HELPER} bazel ${BAZEL_STARTUP_ARGS} build ${BAZEL_BUILD_ARGS} --build_tests_only //test/... -- ${SKIP}
+
+# Run the tests
+${HELPER} bazel ${BAZEL_STARTUP_ARGS} test ${BAZEL_BUILD_ARGS} --build_tests_only --flaky_test_attempts=3 //test/... -- ${SKIP}

ossm/scripts/update-deps.sh
@@ -1,0 +1,146 @@
+#!/bin/bash
+# Copyright Red Hat, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eo pipefail
+
+export CC=gcc CXX=g++ ENVOY_OPENSSL=1
+
+function init(){
+  ROOT_DIR="$(pwd)"
+
+  OUTPUT_BASE="$(mktemp -d)"
+  VENDOR_DIR="${ROOT_DIR}/ossm/vendor"
+  BAZELRC="${ROOT_DIR}/ossm/bazelrc-vendor"
+
+  rm -rf "${OUTPUT_BASE}" &&  mkdir -p "${OUTPUT_BASE}"
+  rm -rf "${VENDOR_DIR}" &&  mkdir -p "${VENDOR_DIR}"
+  : > "${BAZELRC}"
+
+  # Remove symlinks to previous builds to avoid issues
+  rm -f bazel-*
+
+
+  IGNORE_LIST=(
+        "bazel_tools"
+        "envoy_api"
+        "envoy_build_config"
+        "local_config"
+        "local_jdk"
+        "bazel_gazelle_go"
+        "openssl"
+        "go_sdk"
+        "host_platform"
+        "remotejdk"
+        "rust"
+        "nodejs"
+        "rules_foreign_cc_framework_toolchain_freebsd_commands"
+        "rules_foreign_cc_framework_toolchain_macos_commands"
+        "rules_foreign_cc_framework_toolchain_windows_commands"
+        "emscripten"
+        "python3_12_host"
+        "python3_12_x86_64"
+        "python3_12_ppc"
+        "python3_12_s390x"
+        "python3_12_aarch64"
+  )
+}
+
+function error() {
+  echo "$@"
+  exit 1
+}
+
+function validate() {
+  if [ ! -f "WORKSPACE" ]; then
+    error "Must run in the envoy/proxy dir"
+  fi
+}
+
+function contains () {
+  local e match="$1"
+  shift
+  for e; do [[ "$match" == "$e"* ]] && return 0; done
+  return 1
+}
+
+function copy_files() {
+  local cp_flags
+  for f in "${OUTPUT_BASE}"/external/*; do
+    if [ -d "${f}" ]; then
+      repo_name=$(basename "${f}")
+      if contains "${repo_name}" "${IGNORE_LIST[@]}" ; then
+        continue
+      fi
+
+      cp_flags="-rL"
+      if [ "${repo_name}" == "emscripten_toolchain" ]; then
+        cp_flags="-r"
+      fi
+      cp "${cp_flags}" "${f}" "${VENDOR_DIR}" || echo "Copy of ${f} failed. Ignoring..."
+      echo "build --override_repository=${repo_name}=/work/ossm/vendor/${repo_name}" >> "${BAZELRC}"
+    fi
+  done
+
+
+  chmod -R +w "${VENDOR_DIR}"
+  find "${VENDOR_DIR}" -name .git -type d -print0 | xargs -0 -r rm -rf
+  find "${VENDOR_DIR}" -name .gitignore -type f -delete
+  find "${VENDOR_DIR}" -name __pycache__ -type d -print0 | xargs -0 -r rm -rf
+  find "${VENDOR_DIR}" -name '*.pyc' -delete
+}
+
+function run_bazel() {
+  # Workaround to force fetch of rules_license
+  bazel --output_base="${OUTPUT_BASE}" fetch @remote_java_tools//java_tools/zlib:zlib || true
+
+  # Workaround to force fetch of protoc for arm
+  bazel --output_base="${OUTPUT_BASE}" fetch @com_google_protobuf_protoc_linux_aarch_64//:protoc
+
+  bazel --output_base="${OUTPUT_BASE}" fetch @com_github_gperftools_gperftools//:all
+
+  # Fetch all the rest and check everything using "build --nobuild "option
+  for config in x86_64 aarch64 s390x ppc; do
+    bazel --output_base="${OUTPUT_BASE}" build --nobuild --config="${config}" //...
+  done
+}
+
+function patch_python() {
+  local dir repo_name
+
+  for arch in x86_64 s390x ppc64le aarch64; do
+    repo_name="python3_12_${arch}-unknown-linux-gnu"
+    dir="${VENDOR_DIR}/${repo_name}"
+    /bin/rm -rf "${dir}"
+    mkdir -p "${dir}"
+    cp "${ROOT_DIR}/ossm/scripts/BUILD.bazel.python" "${dir}/BUILD.bazel"
+
+    echo "build --override_repository=${repo_name}=/work/ossm/vendor/${repo_name}" >> "${BAZELRC}"
+    echo "workspace(name = \"${repo_name}\")" > "${dir}/WORKSPACE"
+  done
+}
+
+function main() {
+  validate
+  init
+  run_bazel
+  copy_files
+  patch_python
+
+  echo
+  echo "Done. Inspect the result with git status"
+  echo
+}
+
+main