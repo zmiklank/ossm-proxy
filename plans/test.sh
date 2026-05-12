d03c3627c2fbc5df05456f478f460f1dafca2a87
Jose Luis Ojosnegros Manchón <jojosneg@redhat.com>
2026-05-12 14:37:02 +0200

    ci: limit Bazel parallelism to 75% of available CPUs
    Prevents OOM on arm64 bare metal (64 CPUs, 124 GiB RAM) where running
    all cores in parallel during bazel test exhausts available memory.
    75% gives ~2.6 GiB/job headroom vs the ~2 GiB/job that caused OOM.
    Applies dynamically to any machine TF provisions.
    
    Signed-off-by: Jose Luis Ojosnegros Manchón <jojosneg@redhat.com>

--- plans/test.sh
+++ plans/test.sh
@@ -5,7 +5,10 @@ sysctl -w user.max_user_namespaces=15000
 git clone ${SOURCE_REPO} proxy-src
 cd proxy-src
 git checkout ${SOURCE_REF}
+BAZEL_JOBS=$(( $(nproc) * 3 / 4 ))
+
 podman run --rm --privileged \
+  -e BAZEL_JOBS="${BAZEL_JOBS}" \
   -v "$(pwd)":/work:z -w /work \
   quay.io/maistra-dev/maistra-builder:3.3 \
   bash ossm/ci/pre-submit.sh
